8ac5e1d286c4b2bd6d8f75addde421505a90a25c
Owen Cochell <owencochell@gmail.com>
2018-04-05 20:46:56 -0400

    Update to Kahoot bot.py
    Things added:
    1. Put "try" statment for checking connectivity
    2.  Greatly improved efficiency by opening another tab instead of starting another instance
    3. Added more comments

--- Kahoot bot.py
+++ Kahoot bot.py
@@ -1,3 +1,6 @@
+#A simple Kahoot bot that joins Kahoot game and sits idle
+#Version 1.4.3
+#ENTech SS
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -5,31 +8,43 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import random
-print("Kahoot spammer v 1.3.1")
+#Asking for info here:
+print("Kahoot spammer v 1.4.3")
 pin=input("Please enter a game pin:")
 name = input("Please enter a name:")
 join = input("Please enter a amount of bots to join(Default is 50):")
-nameb = 'noname'
+tab = 0
+nameb = 'name'
 bot_num = 1
+#Start chrome
+print("Starting chrome...")
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(chrome_options=chrome_options)
+#If join feild is blank, then default is 50
 if join=='':
     join=50
 def namec():
+    #Code for clarifying name
     global join, bot_num, nameb
     num=random.randint(1,999)*3
     if join=='1':
-        print("Name change not neccessary.")
-    if join>='2':
+        nameb=name
+    if int(join)>=2:
         if bot_num==join:
             print("Name generation completed")
         nameb=(name + '.' + str(num))
         bot_num = bot_num + 1
 def bot():
-    global nameb
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    print("Starting chrome...")
-    #Start chrome
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    global nameb, driver
+    if bot_num==1:
+        print("No new window necessary")
+        continue
+    elseif bot_num >=2:
+        print("Opening new window...")
+        driver.execute_script("window.open('');")
+        driver.switch_to.window(driver.window_handles[tab])
+        continue
     print("Navigating to Kahoot...")
     #Navigate to kahoot.com
     driver.get("https://kahoot.it/")
@@ -48,10 +63,17 @@ def bot():
     namec()
     gname.send_keys(nameb)
     gname.submit()
+    #Checking login
     print("Checking if login was succesfull...")
-    content = driver.find_element_by_class_name('ng-binding')
+    try:
+       content = driver.find_element_by_class_name('ng-binding') 
+    exept:
+        print("Error checking page :("\n"Id could have changed, or connection could have dropped.")
+        x=input("Press any key to exit...")
     print("Success!")
-    print("Bot is now in the game ;)")
+    print("Bot [" + bot_num + "]is now in the game ;)")
+    tab = tab + 1
+#Code for running a set amoun of times
 for x in range(int(join)):
     bot()
 
